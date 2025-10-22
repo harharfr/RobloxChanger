84250aaa10aa358665dadcb297e08020c5351132
harharfr <116351710+harharfr@users.noreply.github.com>
2025-10-22 09:18:05 -0400

    Update Offsets.hpp (Major Update)

--- SpeedChanger/Offsets/Offsets.hpp
+++ SpeedChanger/Offsets/Offsets.hpp
@@ -2,27 +2,25 @@
 #include <Windows.h>
 #include <iostream>
 
-// Version: c1ac69007bdc4e48
+// Version: 7a4a5d7d1fb3449f
 
 namespace Offsets {
-    // Only two that realy change, shouldn't need to change others at all
-
-    inline uintptr_t FakeDatamodelPointer = 0x7097018;
+    inline uintptr_t FakeDatamodelPointer = 0x7268A88;
     inline uintptr_t FakeDatamodeltoRealDatamodel = 0x1C0;
 
-    inline uintptr_t Children = 0x68;
-    inline uintptr_t Name = 0x88;
-    inline uintptr_t Parent = 0x58;
+    inline uintptr_t Children = 0x60;
+    inline uintptr_t Name = 0x80;
+    inline uintptr_t Parent = 0x50;
 
     inline uintptr_t ChildrenEnd = 0x8;
-    inline uintptr_t ObjectValue = 0xD8;
+    inline uintptr_t ObjectValue = 0x100;
 
-    inline uintptr_t LocalPlayer = 0x128;
+    inline uintptr_t LocalPlayer = 0x120;
 
-    inline uintptr_t Walkspeed = 0x1DC;
-    inline uintptr_t WalkspeedCheck = 0x3B8;
-    inline uintptr_t Jumppower = 0x1B8;
-    inline uintptr_t FOV = 0x168;
+    inline uintptr_t Walkspeed = 0x1D4;
+    inline uintptr_t WalkspeedCheck = 0x3A0;
+    inline uintptr_t Jumppower = 0x1B0;
+    inline uintptr_t FOV = 0x160;
 }
 
 namespace Globals {
